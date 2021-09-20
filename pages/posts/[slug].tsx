41ec325a1b92f2d2299361c726afb2e34288040c
Rodrigo D’Agostino <rodrigo.dagostino@globant.com>
2021-09-20 10:54:03 -0300

    Refactor: move fetch logic to a different file

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -9,6 +9,7 @@ import { formatBody, formatDate } from '../../utils/helpers';
 
 import { IBlogPost } from '../../components/BlogPost';
 import classes from '../../styles/Post.module.css';
+import { fetchPaths, fetchPost } from '../../utils/contentful-post';
 
 const Post: NextPage = ({ post }: any) => {
   return (
@@ -61,22 +62,8 @@ const Post: NextPage = ({ post }: any) => {
   );
 };
 
-const spaceId = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY;
-
-const client = createClient({
-  space: spaceId || '',
-  accessToken: accessToken || '',
-});
-
 export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await client.getEntries({ content_type: 'blogPost' });
-
-  const paths = response.items.map((item: any) => {
-    return {
-      params: { slug: item.fields.slug },
-    };
-  });
+  const paths = await fetchPaths();
 
   return {
     paths,
@@ -85,32 +72,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
-  const entries = await client.getEntries({
-    content_type: 'blogPost',
-    'fields.slug': params.slug,
-  });
+  const entry = await fetchPost(params);
 
   return {
-    props: { post: transformContentfulPost(entries.items[0]) },
-  };
-};
-
-/**
- * There are two ways we might be able to solve the “item: any” issue:
- * -1- Use an Interface to identify the fetched content.
- * -2- Ignore the content received.
- */
-const transformContentfulPost = (item: any): IBlogPost => {
-  return {
-    id: item.sys.id,
-    author: item.fields.author,
-    body: formatBody(item.fields.body) || '',
-    categoryName: item.fields.category?.fields.name || '',
-    categorySlug: item.fields.category?.fields.slug || '',
-    datePublished: formatDate(item.sys.createdAt),
-    featuredImageUrl: `https:${item.fields.featuredImage?.fields.file.url}` || '',
-    slug: item.fields.slug,
-    title: item.fields.title,
+    props: { post: entry },
   };
 };
 
